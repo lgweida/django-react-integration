2e4734bd06be89dbbccf9b575de031f1d7c3e722
lgweida <ligu2006@gmail.com>
2023-12-31 16:05:01 +0000

    add message component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,34 @@ const viteLogo = "vite.svg"
 const reactLogoPath = `/static/${reactLogo}`
 const viteLogoPath = `/static/${viteLogo}`
 
+
+function Comment(props : any) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {props.date}
+      </div>
+    </div>
+  );
+}
+
+function Message(prop: any){
+
+  return <h1>{prop.message}</h1>
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -20,6 +48,7 @@ function App() {
           <img src={reactLogoPath} className="logo react" alt="React logo" />
         </a>
       </div>
+      <Message message="Hi"/>
       <h1>Vite + React + Django</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
