5620bc29d233f7bb9472ef58b5140d2a235dad10
lgweida <ligu2006@gmail.com>
2023-12-31 16:13:42 +0000

    fix count

--- src/App.tsx
+++ src/App.tsx
@@ -57,6 +57,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <Message message={count}/>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
